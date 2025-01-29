986d8ccff0220cc12269d7b7352a555cf360e6c9
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-01-29 08:52:57 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -5,37 +5,32 @@ const dotenv = require("dotenv");
 const connectDb = require("./config/db");
 const path = require("path");
 
-// rest object
 const app = express();
 
-// middleware
 app.use(express.json());
 app.use(morgan("dev"));
 
 dotenv.config();
 
-// mongo connection
 connectDb();
 
-// routes
+// Routes for your API
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/feedback", require("./routes/feedBackRoute"));
 app.use("/uploads", express.static("uploads"));
 
+// Serve static files from React
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-// Catch-all route for any unmatched request to serve the React app
+// Catch-all route for React
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const port = process.env.PORT || 8080;
 
-// listen on port
 app.listen(port, () => {
-  console.log(
-    `Server running on development mode ${process.env.PORT}`.bgYellow.black
-  );
+  console.log(`Server running on port ${port}`.bgYellow.black);
 });

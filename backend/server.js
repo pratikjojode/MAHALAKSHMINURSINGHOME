095455c20b7b0bc2968ff7650189f35bd14b8a8a
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-01-29 08:13:33 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDb = require("./config/db");
+const path = require("path");
 // rest object
 const app = express();
 
@@ -12,20 +13,24 @@ app.use(morgan("dev"));
 
 dotenv.config();
 
-// mongo connection
+// mongoconnc
 connectDb();
-
 // routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/feedback", require("./routes/feedBackRoute"));
 app.use("/uploads", express.static("uploads"));
-
 // port
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 const port = process.env.PORT || 8080;
 
-// listen on port
+// listen the port
 app.listen(port, () => {
   console.log(
     `Server running on development mode ${process.env.PORT}`.bgYellow.black

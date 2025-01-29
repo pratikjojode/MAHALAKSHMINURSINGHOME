41e9da5650f2a6f7540ff3ac94b03fc67a7a10ae
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-01-29 08:08:00 +0530

    Updated backend code for deployment on Render

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDb = require("./config/db");
-const path = require("path");
 // rest object
 const app = express();
 
@@ -13,24 +12,20 @@ app.use(morgan("dev"));
 
 dotenv.config();
 
-// mongoconnc
+// mongo connection
 connectDb();
+
 // routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/feedback", require("./routes/feedBackRoute"));
 app.use("/uploads", express.static("uploads"));
+
 // port
-
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 const port = process.env.PORT || 8080;
 
-// listen the port
+// listen on port
 app.listen(port, () => {
   console.log(
     `Server running on development mode ${process.env.PORT}`.bgYellow.black

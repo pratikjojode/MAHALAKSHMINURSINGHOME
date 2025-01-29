f878c1195968142f3eb251908d373fdc0e7faedf
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-01-29 08:40:00 +0530

    Move client/build folder to backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDb = require("./config/db");
 const path = require("path");
+
 // rest object
 const app = express();
 
@@ -13,25 +14,26 @@ app.use(morgan("dev"));
 
 dotenv.config();
 
-// mongoconnc
+// mongo connection
 connectDb();
+
 // routes
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/feedback", require("./routes/feedBackRoute"));
 app.use("/uploads", express.static("uploads"));
-// port
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// Catch-all route for any unmatched request to serve the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const port = process.env.PORT || 8080;
 
-// listen the port
+// listen on port
 app.listen(port, () => {
   console.log(
     `Server running on development mode ${process.env.PORT}`.bgYellow.black
